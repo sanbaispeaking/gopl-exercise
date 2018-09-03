fd24602549afd79c140ff9c746cc75458548c639
Sanbai <wangzhihao1030@gmail.com>
2018-09-03 23:58:38 +0800

    Exercise 8.13 logout idle client

---
+++ ch8/8.13/chat.go
@@ -0,0 +1,118 @@
+// Chat is a server that lets clients chat with each other.
+package main
+
+import (
+	"time"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+//!+broadcaster
+
+type namedClient struct {
+	ch  chan<- string // an outgoing message channels
+	who string
+}
+
+var (
+	entering = make(chan namedClient)
+	leaving  = make(chan namedClient)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[namedClient]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli.ch <- msg
+			}
+
+		case cli := <-entering:
+			if len(clients) > 0 {
+				others := "Online:\n"
+				for client := range clients {
+					others += ("\t" + client.who + "\n")
+				}
+				cli.ch <- others
+			}
+			clients[cli] = true
+			// Display current active clients to new arrival
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+//!-broadcaster
+
+//!+handleConn
+func handleConn(conn net.Conn) {
+	// setup a client for new commer
+	ch := make(chan string) // outgoing client messages
+	who := conn.RemoteAddr().String()
+	client := namedClient{ch, who}
+	go clientWriter(conn, ch)
+
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- client
+
+	buffer := make(chan string) // buffering data reading from conn
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		buffer <- input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	go func(){
+		for {
+			select {
+			case <- time.After(5 * time.Second):
+				conn.Close()
+				return
+			case raw := <- buffer:
+					messages <- who + ": " + raw
+			}
+		}
+	}()
+
+	leaving <- client
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
+
+//!-handleConn
+
+//!+main
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+//!-main
