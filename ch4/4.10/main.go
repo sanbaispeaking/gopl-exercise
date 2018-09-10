c865c8864dcd382f3d1236e9276b80cf91431e7b
Sanbai <wangzhihao@nutstore.net>
2018-09-10 20:48:18 +0800

    Exercise 4.10 group issues by time of creation

---
+++ ch4/4.10/main.go
@@ -0,0 +1,73 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 112.
+//!+
+
+// Issues prints a table of GitHub issues matching the search terms.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"time"
+
+	"gopl.io/ch4/github"
+)
+
+//!+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	now := time.Now()
+	var lastMonth, lastYear int
+	sort.Slice(result.Items, func(i, j int) bool { return result.Items[i].CreatedAt.After(result.Items[j].CreatedAt) })
+
+	for index, item := range result.Items {
+		if item.CreatedAt.After(now.AddDate(0, -1, 0)) {
+			lastMonth = index
+		} else if item.CreatedAt.After(now.AddDate(-1, 0, 0)) {
+			lastYear = index
+		}
+	}
+
+	fmt.Printf("  last month:\n")
+	for index, item := range result.Items {
+		fmt.Printf("#%-5d %s %9.9s %.55s\n", item.Number, item.CreatedAt.Format(time.UnixDate), item.User.Login, item.Title)
+		if index == lastMonth {
+			fmt.Printf("  last year:\n")
+		}
+		if index == lastYear {
+			fmt.Printf("  a year ago:\n")
+		}
+	}
+}
+
+//!-
+
+/*
+//!+textoutput
+$ go build gopl.io/ch4/issues
+$ ./issues repo:golang/go is:open json decoder
+13 issues:
+#5680    eaigner encoding/json: set key converter on en/decoder
+#6050  gopherbot encoding/json: provide tokenizer
+#8658  gopherbot encoding/json: use bufio
+#8462  kortschak encoding/json: UnmarshalText confuses json.Unmarshal
+#5901        rsc encoding/json: allow override type marshaling
+#9812  klauspost encoding/json: string tag not symmetric
+#7872  extempora encoding/json: Encoder internally buffers full output
+#9650    cespare encoding/json: Decoding gives errPhase when unmarshalin
+#6716  gopherbot encoding/json: include field name in unmarshal error me
+#6901  lukescott encoding/json, encoding/xml: option to treat unknown fi
+#6384    joeshaw encoding/json: encode precise floating point integers u
+#6647    btracey x/tools/cmd/godoc: display type kind of each named type
+#4237  gjemiller encoding/base64: URLEncoding padding is optional
+//!-textoutput
+*/
