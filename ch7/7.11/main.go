9bcb24b4e8a3cdcc0eaeb05bf2a430912452d857
Sanbai <wangzhihao@nutstore.net>
2018-09-14 20:42:57 +0800

    Exercise 7.11 pesudo server supports update

---
+++ ch7/7.11/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//!+main
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/create", db.create)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//!-main
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
+
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; ok {
+		msg := fmt.Sprintf("item exists: %q\n", item)
+		http.Error(w, msg, http.StatusForbidden)
+		return
+	}
+
+	p, err := strconv.Atoi(price)
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q\n", price)
+		http.Error(w, msg, http.StatusForbidden)
+		return
+	}
+
+	db[item] = dollars(p)
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "created item: %q\n", item)
+}
