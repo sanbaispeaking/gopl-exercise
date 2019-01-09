30c78ed721a0acada4adfa80ab83eea02adf9613
Sanbai <wangzhihao@nutstore.net>
2019-01-09 18:32:51 +0800

    ftp server supports cd command

--- ch8/8.2/ftp.go
+++ ch8/8.2/ftp.go
@@ -30,22 +30,38 @@ func main() {
 func handleConn(c net.Conn) {
 	defer c.Close()
 	input := bufio.NewScanner(c)
-	cwd, _ := os.Getwd()
+	fmt.Fprintln(c, "version 0.1")
 
 	for input.Scan() {
-		text := input.Text()
-		pieces := strings.Split(text, " ")
-		cmd := pieces[0]
+		cwd, _ := os.Getwd()
+		args := strings.Split(input.Text(), " ")
+		cmd := args[0]
+		args = args[1:]
 
 		switch cmd {
+
 		case "ls":
 			files, _ := ioutil.ReadDir(cwd)
 			for _, f := range files {
 				fmt.Fprintf(c, "%s\n", f.Name())
 			}
+
 		case "cd":
-			dir := filepath.Join(cwd, pieces[1])
-			os.Chdir(dir)
+			if len(args) > 0 {
+				dir := filepath.Join(cwd, args[0])
+				if err := os.Chdir(dir); err != nil {
+					if os.IsPermission(err) {
+						fmt.Fprintln(c, err)
+					}
+				}
+			}
+
+		case "get":
+			if len(args) < 1 {
+				fmt.Fprintln(c, "missing file operand")
+				break
+			}
+
 		case "exit":
 			fmt.Fprintf(c, "Bye.\n")
 			return
