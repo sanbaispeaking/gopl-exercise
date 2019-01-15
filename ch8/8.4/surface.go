015892d7ac6a8c966e4c62da5a3e59ea71c45398
Sanbai <wangzhihao@nutstore.net>
2019-01-15 20:02:55 +0800

    Fix Exer. 8.4 use a fixed number of goroutines

--- ch8/8.4/surface.go
+++ ch8/8.4/surface.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"sync"
 )
@@ -18,58 +17,64 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-func syncGenSurface() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+func sGenSurface() {
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			// ax, ay := corner(i+1, j)
+			// bx, by := corner(i, j)
+			// cx, cy := corner(i, j+1)
+			// dx, dy := corner(i+1, j+1)
+			// fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			// 	ax, ay, bx, by, cx, cy, dx, dy)
+			corner(i+1, j)
+			corner(i, j)
+			corner(i, j+1)
+			corner(i+1, j+1)
+
 		}
 	}
-
-	fmt.Println("</svg>")
 }
 
-func main() {
-
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-
-	point := make(chan string)
-	var wg sync.WaitGroup
-
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			wg.Add(1)
-			go func(xindex, yindex int) {
-				defer wg.Done()
-				ax, ay := corner(xindex+1, yindex)
-				bx, by := corner(xindex, yindex)
-				cx, cy := corner(xindex, yindex+1)
-				dx, dy := corner(xindex+1, yindex+1)
-				point <- fmt.Sprintf("<polygon points='%f,%f %f,%f %f,%f %f,%f'/>\n",
-					ax, ay, bx, by, cx, cy, dx, dy)
-			}(i, j)
-		}
+func aGenSurfaceFixedN(workerNum int) {
+	type item struct {
+		xindex int
+		yindex int
 	}
 
+	items := make(chan item, cells*cells)
+
+	// populate input channel
 	go func() {
-		wg.Wait()
-		close(point)
+		for i := 0; i < cells; i++ {
+			for j := 0; j < cells; j++ {
+				items <- item{xindex: i, yindex: j}
+			}
+		}
+		defer close(items)
 	}()
 
-	for p := range point {
-		fmt.Printf(p)
+	var wg sync.WaitGroup
+	for i := 0; i < workerNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for item := range items {
+				xindex, yindex := item.xindex, item.yindex
+				// ax, ay := corner(xindex+1, yindex)
+				// bx, by := corner(xindex, yindex)
+				// cx, cy := corner(xindex, yindex+1)
+				// dx, dy := corner(xindex+1, yindex+1)
+				// fmt.Printf("<polygon points='%f,%f %f,%f %f,%f %f,%f'/>\n",
+				// 	ax, ay, bx, by, cx, cy, dx, dy)
+				corner(xindex+1, yindex)
+				corner(xindex, yindex)
+				corner(xindex, yindex+1)
+				corner(xindex+1, yindex+1)
+			}
+		}()
 	}
 
-	fmt.Println("</svg>")
+	wg.Wait()
 }
 
 func corner(i, j int) (float64, float64) {
@@ -90,3 +95,38 @@ func zindex(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
 }
+
+func main() {
+	// aGenSurfaceFixedN(8)
+	sGenSurface()
+}
+
+// Worker = 2
+// real	0m0.245s
+// user	0m0.282s
+// sys	0m0.055s
+
+// Worker = 3
+// real	0m0.279s
+// user	0m0.295s
+// sys	0m0.042s
+
+// Worker = 4
+// real	0m0.271s
+// user	0m0.266s
+// sys	0m0.063s
+
+// Worker = 6
+// real	0m0.284s
+// user	0m0.332s
+// sys	0m0.036s
+
+// Worker = 8
+// real	0m0.268s
+// user	0m0.269s
+// sys	0m0.055s
+
+// Single goroutine
+// real	0m0.289s
+// user	0m0.291s
+// sys	0m0.066s
